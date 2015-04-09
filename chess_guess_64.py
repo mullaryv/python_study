ae47d9d7f024cf56ee10fb27bf30f1792b3cf3df
mullaryv <mullaryv@yahoo.com>
2015-04-09 19:00:42 -0400

    fixed bugs, added test

--- chess_guess_64.py
+++ chess_guess_64.py
@@ -8,37 +8,45 @@ After that, another person with no prev. knowledge of a board
 should tell which square was selected.
 (Two persons above may communicate before the board configuration is created)
 '''
-import math
 
-# validate individual square
+import math
+from functools import reduce
+
+# validate individual square, should be from ['A1',...'H8']
 def isValid (square) :
   return (len(square) == 2) and \
          (ord(square[0]) in range (ord('A'), ord('I'))) and \
          (int(square[1]) in range(1,9))
 
-# convert a valid 2-char square to an ordinal from [A1=1,.., H8=64]
+
+# convert a valid 2-char square to an ordinal from [A1=1,A2=9,...H8=64]
 def toOrdinal (square) :
-  #return (ord(square[0])-ord('A'))*8 + int(square[1])
   return (ord(square[0])-ord('A') +1) + (int(square[1])-1)*8
-#print (toOrdinal('H7'))
-
-  
-# converts 6-bit (integer) representation to a 2-char square  
-def toSquare (bit6) :
-  return chr ((bit6>>3) + ord('A')) + str (bit6 % 8 + 1)
 
 
-# XOR sum of all valid elements in the list
+# converts an ordinal (integer) to a 2-char square name
+def toSquare (ordinal) :
+  r = ordinal % 8
+  num = 1 + math.floor(ordinal / 8)
+  if r == 0:
+    num -= 1
+  let = 'H'
+  if r > 0: 
+    let= chr(ord ('A') + r - 1)
+  return let + str(num)
+
+
+# sum of all numbered squares in the list
 def sum64 (lst) :
   s = 0
   for square in lst:
-    if (isValid (square)):
-      s += toOrdinal(square)
+    s += toOrdinal(square)
   return s
   
   
 # read chess board configuration
-board = "a2 a4 b7 c8 f3 g3 h5 h6"
+#board = "a2 a4 b7 c8 f3 g3 h5 h6"
+board = "c5"
 # some corner cases: [b1,f6] (b1 selected); [f6] (a1); [c5] (c5)
 
 # process board configuration: convert to upper string, validate
@@ -64,79 +72,55 @@ if not isValid(sel_str):
 sel_square = toOrdinal (sel_str)  #ordinal number of selected square
 
 sub_board = 2080 - board_sum
-rem = sub_board - math.floor (sub_board / 64)*64
-#rem = math.floor (math.fmod (sub_board, 64))
+#rem = sub_board - math.floor (sub_board / 64)*64
+rem = math.floor (sub_board % 64)
 
-print ("initial board: ", marbles, "  natural sum: ", board_sum, "  remainder: ", rem)
-print ("\n  selected: " + sel_str + "  (" + str(sel_square) + ")")
+print ("\n  initial board: ", board_lst, "  selected = " + sel_str + " (" + str(sel_square) + ")")
+print ("  numbered board: ", marbles, "  (sum:", board_sum, "  remainder:", rem, ")")
 
+#print ("\n  selected: " + sel_str + "  (" + str(sel_square) + ")")
 
-num_1 = math.fabs(rem-sel_square)
-num_2 = 64 - num_1
-
+# we can either add or remove a marble (both numbers are from [1..64])
+num_add = int (math.fabs(rem - sel_square))
+num_remove = 64 - num_add
+if (sel_square > rem):  #swap
+  t = num_remove
+  num_remove = num_add
+  num_add = t  
+print ("    candidates to add/remove: [" + str(num_add) + "," + str (num_remove) + "]")
+  
 
 #modify board, if needed
 done = False
 
-if (sel_square == rem):
-  print ("  modify board: not needed")
+if (sel_square == rem): #nothing to do  
+  print ("    no need to modify")
   done = True
-
-if (sel_square > rem):
-  if num_1 in (marbles):              #remove
-    marbles = marbles.remove(num_1)
-    print ("  modify board: removed: ", num_1)
+# else try to modify board
+else:
+  if num_add not in (marbles):           #add
+    marbles.append (num_add)
+    print ("    added:", num_add)
     done = True
-  else:                               #add
-    if num_2 not in (marbles):
-      marbles = marbles.append (num_2)
-      print ("  modify board: added: ", num_2)
-      done = True
-
-if (sel_square < rem):
-  if num_1 not in (marbles):          #add
-    marbles = marbles.append (num_1)
-    print ("  modify board: added: ", num_1)
-    done = True
-  else:                               #remove
-    if num_2 in (marbles):
-      marbles = marbles.remove (num_2)
-      print ("  modify board: removed: ", num_2)
+  else:                                  #remove
+    if num_remove in (marbles):
+      marbles.remove (num_remove)
+      print ("    removed:", num_remove)
       done = True
 
 if done:
-  # check
-  new_sum = XORsum (board_lst)  #XOR sum of all marbles
-  print ("\nTEST:  choosen square=" + toSquare (new_sum))
-
+  print ("  modified board: " + str (marbles))
+#  print ("              or: " + str (marbles))
 else:
-  print ("  !!!error: cannot do anything: ", num_1, ", ", num2)
+  print ("  !error: cannot do anything: ", num_1, ", ", num2)
+  exit (-100)
+  
+
+# check
+new_sub_board = 2080 - reduce(lambda x, y: x+y, marbles)  #direct sum of all numbered squares in modified list
+rem_test = math.floor (new_sub_board % 64)
+print ("\nTEST:  board: " + str (marbles), "  (sum:", new_sub_board, ", remainder:", rem_test,")")
+print ("TEST:    selected " + sel_str + " == " + toSquare (rem_test) + " --> " + str(sel_str==toSquare (rem_test)))
+
+
         
-
-      
-'''
-board_sum_plus = board_sum ^ sel_bits
-new_square= toSquare (board_sum_plus)
-
-print ("\n  selected: " + sel_str + "  (" + str(sel_bits) + ")")
-print ("  board XOR updated: " + str (board_sum_plus))
-print ("  square to change: " + new_square)
-
-# If there already exists a marble on the new square in the original board,
-# then we should remove it, instead of adding.
-# Also special case is when we don't want to change anything on the board
-#   (which is the same as putting/removing a marble to/from A1 field)
-
-move = "     (.)" # by default, do nothing
-if (new_square in board_lst):
-  board_lst[board_lst.index(new_square)] = "  "  #replace (vs. remove), for nice printing
-  move = "     (-" + new_square + ")"
-else:
-  if (board_sum_plus !=0):
-    move = "  (+" + new_square + ")"
-    board_lst.append (new_square)
-
-print ("original board: " + board + "     (XOR: " + str(board_sum) + ")")
-print ("updated board:  " + " ".join (board_lst) + move) 
-
-'''
